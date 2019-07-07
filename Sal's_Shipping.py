3b94d96373d6e04850b7fab624f43956b6aa415c
Miguel <miiguelb07@gmail.com>
2019-07-06 20:34:13 -0500

    Finished the function that calculates the shipping cheapest method, "cheap_method"

--- Sal's_Shipping.py
+++ Sal's_Shipping.py
@@ -1,30 +1,36 @@
 premium = 125.00
+weight = 17.0
 
 # Ground Shipping
-def ground_shipping(weight, cost):
+def ground_shipping(weight):
   if weight <= 2:
-    return (weight * 1.50 + cost)
+    return (weight * 1.50 + 20)
   elif 2 < weight <= 6:
-    return (weight * 3.00 + cost)
+    return (weight * 3.00 + 20)
   elif 6 < weight <= 10:
-    return (weight * 4.00 + cost)
+    return (weight * 4.00 + 20)
   else:
-    return (weight * 4.75 + cost)
+    return (weight * 4.75 + 20)
   
 # Drone Shipping
-def drone_shipping(weight, cost):
+def drone_shipping(weight):
   if weight <= 2:
-    return (weight * 4.50 + cost)
+    return (weight * 4.50 + 0)
   elif 2 < weight <= 6:
-    return (weight * 9.00 + cost)
+    return (weight * 9.00 + 0)
   elif 6 < weight <= 10:
-    return (weight * 12.00 + cost)
+    return (weight * 12.00 + 0)
   else:
-    return (weight * 14.25 + cost)
+    return (weight * 14.25 + 0)
 
 # Cheapest Method of shipping
+def cheap_method(weight):
+  if ground_shipping(weight) < drone_shipping(weight) and ground_shipping(weight) < premium:
+    return "Ground shipping is the cheapest method, and it costs: " + str(ground_shipping(weight))
+  elif drone_shipping(weight) < ground_shipping(weight) and drone_shipping(weight) < premium:
+    return "Drone shipping is the chepeast method, and it costs: " + str(drone_shipping(weight))
+  elif premium < ground_shipping(weight) and premium < drone_shipping(weight):
+    return "Premium ground shipping is the cheapest method, and it costs: " + str(premium)
 
 
-
-print(ground_shipping(8.4, 20))
-print(drone_shipping(1.5, 0))
+print(cheap_method(weight))
