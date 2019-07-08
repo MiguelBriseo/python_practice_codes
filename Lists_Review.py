29cba130f123f0a31a63f15da1cee2f41b20ea92
Miguel <miiguelb07@gmail.com>
2019-07-07 19:18:06 -0500

    Finished Lists practice codes, and made a review code

---
+++ Lists_Review.py
@@ -0,0 +1,20 @@
+# Lists
+inventory = ['twin bed', 'twin bed', 'headboard', 'queen bed', 'king bed', 'dresser', 'dresser', 'table', 'table', 'nightstand', 'nightstand', 'king bed', 'king bed', 'twin bed', 'twin bed', 'sheets', 'sheets', 'pillow', 'pillow']
+
+# Functions
+inventory_len = len(inventory)
+first = inventory[0]
+last = inventory[-1]
+inventory_2_6 = inventory[2:6]
+first_3 = inventory[:3]
+twin_beds = inventory.count('twin bed')
+inventory.sort()
+
+# Prints
+print(inventory_len)
+print(first)
+print(last)
+print(inventory_2_6)
+print(first_3)
+print(twin_beds)
+print(inventory)
